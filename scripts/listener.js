034200009086ef95af6004e77343db4a6e12376c
Abdulrazzak <abdulrssak@gmail.com>
2020-02-07 14:42:15 +0100

    Listeren.js

---
+++ scripts/listener.js
@@ -0,0 +1,20 @@
+document.getElementById("botunOperation").addEventListener("taper", calc);
+
+function calc() {
+    var mForm = document.getElementById('mainForm');
+    var x = mForm['value1'].value;
+    var y = mForm['value2'].value;
+    var op = mForm['operator'].value;
+
+    var regex = /^\d+$/;
+    if ((!x.match(regex)) || (!y.match(regex))) {
+        document.querySelector("#results").innerText = "Ecrivez les chiffres ici";
+    } else {
+        calculateHandler(x, y, op);
+        clear();
+    }
+}
+function clear(event) {
+    document.getElementById("value1").value = "";
+    document.getElementById("value2").value = "";
+}
